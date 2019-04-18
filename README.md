9cdf044854465147345b8ef818df623d860fa81d
bketen <b.batuhanketen@gmail.com>
2019-04-18 03:02:01 +0300

    Project is added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# FormApplication
+##Form oluşturma web sayfası
+Angularjs 1.6.9 sürümü ile yazılmış olup bootstrap ile desteklenmiştir.
+
+#Note:
+Çalıştırılması için bir server üzerinde çalışması gerekmektedir. Localde forms.html'in görüntülenirken oluşan "CORS" hatasını çözmek için böyle bir çözüm uygulandı. Örnek olaram WAMP server kullanılmıştır. Localhost dahil herhangibir http içeren link üzerinde çalışacaktır.
---
+++ app/app.js
@@ -0,0 +1,105 @@
+let example = angular.module("example", ['ngStorage','ngRoute']);
+example.controller("ExampleController", function($scope, $localStorage) {
+
+    if($localStorage.forms){
+        $scope.datas = JSON.parse($localStorage.forms).forms;
+    }
+
+});
+
+
+example.controller("ModalController", function ($scope, $localStorage) {
+
+        $scope.originalForm = {
+            formName: 'Form1',
+            description: 'Uye formu',
+            date: new Date,
+            name: 'Batuhan',
+            surname: 'Keten',
+            age: 25
+        };
+
+        $scope.form = angular.copy($scope.originalForm);
+
+        $scope.submitForm = function () {
+            let object = {
+                forms:[]
+            }
+
+            tmpDate = new Date();
+
+            
+            $scope.form.date = tmpDate.getFullYear() + "-" + tmpDate.getMonth() + "-" + tmpDate.getDate();
+            console.log($scope.form.date);
+
+            if($localStorage.forms){
+                let a = JSON.parse($localStorage.forms)
+                console.log(a.forms);
+                a.forms.push($scope.form);
+                $localStorage.forms = JSON.stringify(a); 
+            } else {
+                object.forms.push($scope.form);
+                $localStorage.forms = JSON.stringify(object)
+            }
+
+            console.log(JSON.parse($localStorage.forms)); 
+            alert("Başarılı Kayıt");
+
+        };
+
+        //6. create resetForm() function. This will be called on Reset button click.  
+        $scope.resetForm = function () {
+            //$scope.form = angular.copy($scope.originalForm);
+            $localStorage.$reset();
+        };
+
+});
+
+example.config(function ($routeProvider) {
+             
+    $routeProvider.when('/forms/:formName', {
+        templateUrl: 'forms.html',
+        controller: 'FormController'
+    }).otherwise({
+        redirectTo: "/"
+    });
+
+});
+
+example.controller("FormController", function ($scope, $location, $localStorage, $routeParams) {
+    
+    $scope.originalForm = {
+        formName: 'Form1',
+        description: 'Uye formu',
+        date: new Date,
+        name: 'Batuhan',
+        surname: 'Keten',
+        age: 25
+    };
+    username = $routeParams.formName
+    console.log($localStorage.forms)
+    if($localStorage.forms){
+        let array = JSON.parse($localStorage.forms).forms;
+        angular.forEach(array, function (value, key){
+            if(value.formName == username){
+                $scope.originalForm.formName = value.formName;
+                $scope.originalForm.description = value.description;
+                $scope.originalForm.date = value.date;
+                $scope.originalForm.name = value.name;
+                $scope.originalForm.surname = value.surname;
+                $scope.originalForm.age = value.age;
+            }
+        })
+    }
+
+
+
+
+    $scope.form = angular.copy($scope.originalForm);
+
+
+
+    $location.path('/forms/' + username)
+    
+
+});
